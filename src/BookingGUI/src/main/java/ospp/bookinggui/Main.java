9a2aa6fbb206ba30f4c3e8ce9a56295c5bb7dd2b
Lucas Arnström <lucasarnstrom@gmail.com>
2015-04-23 16:11:22 +0200

    Created basic message class

--- src/BookingGUI/src/main/java/ospp/bookinggui/Main.java
+++ src/BookingGUI/src/main/java/ospp/bookinggui/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		//TODO start WORKING YOU FOOL!
+
+		// Create message queue
+		// Initiate message listener
+		// Initiate message sender
+		// Initiate GUI
 	}
 }
---
+++ src/BookingGUI/src/main/java/ospp/bookinggui/Message.java
@@ -0,0 +1,45 @@
+package ospp.bookinggui;
+
+public abstract class Message {
+
+	public enum Type {
+
+		GET_PASSENGERS((byte) 1),
+		GET_PASSENGERS_RESP((byte) 2),
+		BOOK_SEAT((byte) 3),
+		BOOK_SEAT_RESP((byte) 4),
+		LOGIN((byte) 5),
+		LOGIN_RESP((byte) 6),
+		DISCONNECT((byte) 7),
+		HEARTBEAT((byte) 8);
+
+		public final byte ID;
+
+		private Type(byte id) {
+			this.ID = id;
+		}
+	}
+
+	private final Type TYPE;
+	private final byte MESSAGE_LENGTH;
+	private final byte[] MESSAGE;
+
+	protected Message(Type t, byte[] m) {
+		this.TYPE = t;
+		this.MESSAGE_LENGTH = (byte) (2 + m.length);
+		this.MESSAGE = m;
+	}
+
+	public byte[] constructMessage() {
+		byte[] message = new byte[this.MESSAGE_LENGTH];
+
+		message[0] = this.TYPE.ID;
+		message[1] = this.MESSAGE_LENGTH;
+
+		for(int i = 2; i < this.MESSAGE_LENGTH; i++) {
+			message[i] = this.MESSAGE[i - 2];
+		}
+
+		return message;
+	}
+}
