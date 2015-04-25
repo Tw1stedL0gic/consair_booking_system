8d9dc7e90562f68c646ae8925d5f28f78cc6beae
andreasrubensson <andreas.rubensson.8748@student.uu.se>
2015-04-25 13:59:10 +0200

    Copied the Loop function in book.erl to Server.erl

--- src/Server.erl
+++ src/Server.erl
@@ -62,3 +62,81 @@ loop(Listen) ->
 %% function. Maybe listen_loop or something. - Carl
 
 %%--------------------------------------------------------------%%
+
+
+%% @doc TODO: add documentation
+-spec loop(Input) -> ok when 
+      Input::[integer()|string()].
+
+
+loop(Input) ->
+	receive 
+		{1, Package, PID} -> %receive Id=1 with PID_1
+			case validate({1,Package}) of
+				false ->
+					print_Bad_Argument, loop(Input);
+				true -> get({PassengerList, PID}), loop(Input)
+			end;
+		{2, Package, PID} -> % receive XXXXXXXX, do YYYYYYYYYY
+			case validate({2,Package}) of
+				false ->
+					print_Bad_Argument;
+				true -> PID ! {PassengerList, Package}
+			end;
+			loop(Input);
+		{3, Package, PID} ->  % receive XXXXXXXX, do YYYYYYYYYY
+			case validate({3,Package}) of
+				false ->
+					print_Bad_Argument, loop(Input);
+				true -> get({BookSeat, PID}), loop(Input)
+			end;
+		{4, Package, PID} ->  % receive XXXXXXXX, do YYYYYYYYYY
+			case validate({4,Package}) of
+				false ->
+					print_Bad_Argument;
+				true -> PID ! {BookSeat, Package}
+			end			
+			loop(Input);
+		{5, Package, PID} ->  % receive XXXXXXXX, do YYYYYYYYYY
+			case validate({5,Package}) of
+				false ->
+					print_Bad_Argument, loop(Input);
+				true -> login({Login, PID}), loop(Input)
+			end;
+		{6, Package, PID} ->  % receive XXXXXXXX, do YYYYYYYYYY
+			case validate({6,Package}) of
+				false ->
+					print_Bad_Argument;
+				true -> PID ! {Login, Package}
+			end			
+			loop(Input);
+		{7, Package, PID} ->  % receive XXXXXXXX, do YYYYYYYYYY		 
+			case validate({7,Package}) of
+				false ->
+					print_Bad_Argument;
+				true -> disconnect({Disconnect, PID})
+			end
+			loop(Input);
+		{8, Package, PID} ->  % receive XXXXXXXX, do YYYYYYYYYY
+			case validate({8,Package}) of
+				false ->
+					print_Bad_Argument;
+				true -> heartbeat({Heartbeat, PID})
+			end
+			loop(Input);
+		{9, Package, PID} ->  % receive XXXXXXXX, do YYYYYYYYYY
+			case validate({9,Package}) of
+				false ->
+					print_Bad_Argument, loop(Input);
+				true -> get({PassengerInfo, PID}), loop(Input)
+			end;
+		{10, Package, PID} ->  % receive XXXXXXXX, do YYYYYYYYYY
+			case validate({10,Package}) of
+				false ->
+					print_Bad_Argument;
+				true -> PID ! {PassengerInfo, Package}
+			end			
+			loop(Input);
+	end.
+		
+		
