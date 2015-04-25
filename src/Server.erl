cf16a80c870a8f9adba42e5cadc0b0b57239e1e0
Carl Wingardh <c.a.wingardh@gmail.com>
2015-04-25 12:20:59 +0200

    Formatted Server.erl to fit code standard. Added som documentation

--- src/Server.erl
+++ src/Server.erl
@@ -1,20 +1,37 @@
 
-%%Handels the server io for the booking. This is the abstration layer for java / erlang%%
+%%Handles the server io for the booking. This is the abstraction layer for java / erlang%%
 
 -module('Server').
 -export([start/0, loop0/1]).
 -define(PORTNO, 33333).
 
+%%--------------------------------------------------------------%%
+
+%% @doc The start function, used to start a loop which listens to a
+%% certain port number.
+-spec start() -> ok.
+
 start ()->
     start(?PORTNO).
+
+
+%% @doc Auxilary start function 
+-spec start(Pno) -> ok when
+      Pno::integer().
+
 start(Pno) ->
     spawn(?MODULE, loop0, [Pno]).
 
-loop0(Port)->
-    case gen_tcp:listen(Port, [binary,{packet,0},{activ,false}]) of 
-	{ok, LSock}->
+%%--------------------------------------------------------------%%
+%%--------------------------------------------------------------%%
+
+%% @doc loop
+
+loop0(Port) ->
+    case gen_tcp:listen(Port, [binary,{packet,0},{active,false}]) of 
+	{ok, LSock} ->
 	    loop(LSock);
-	_ ->
+ 	_           ->
 	    stop
     end.
 
@@ -24,6 +41,8 @@ loop(Listen) ->
 	    gen_tcp:send(S, io_lib:format("~p~n",[{date(),time()}])),
 	    gen_tcp:close(S),
 	    loop(Listen);
-	_ ->
+	_      ->
 	    loop(Listen)
     end.
+
+%%--------------------------------------------------------------%%
