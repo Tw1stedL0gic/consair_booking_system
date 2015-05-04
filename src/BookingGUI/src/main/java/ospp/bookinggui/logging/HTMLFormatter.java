b1bf3b34af52b4b1fd63df5c0b6f26e411cb7795
Chilinot <lucasarnstrom@gmail.com>
2015-05-04 15:59:04 +0200

    Added some basic logging formatters

---
+++ src/BookingGUI/src/main/java/ospp/bookinggui/logging/HTMLFormatter.java
@@ -0,0 +1,48 @@
+package ospp.bookinggui.logging;
+
+import java.util.Date;
+import java.util.logging.Formatter;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+
+/**
+ * Used by the handler for the log file to format it according to the HTML syntax.
+ */
+public class HTMLFormatter extends Formatter {
+
+	/**
+	 * Formats the given record to an html string.
+	 * @param record - The record to format.
+	 * @return - String representing the formatted record.
+	 */
+	public String format(LogRecord record) {
+		StringBuilder builder = new StringBuilder();
+
+		builder.append("<tr>");
+		builder.append("<td>").append(record.getLevel()).append("</td>");
+		builder.append("<td>").append((new Date(record.getMillis())).toString()).append("</td>");
+		builder.append("<td>").append(record.getLoggerName()).append("</td>");
+		builder.append("<td>").append(record.getSourceMethodName()).append("</td>");
+		builder.append("<td>").append(record.getMessage()).append("</td>");
+
+		builder.append("<td>");
+		if(record.getThrown() != null) {
+			for(StackTraceElement trace : record.getThrown().getStackTrace()) {
+				builder.append(trace).append("</br>");
+			}
+		}
+		builder.append("</td>");
+
+		builder.append("</tr>\n");
+
+		return builder.toString();
+	}
+
+	public String getHead(Handler h) {
+		return ("<html>\n <body>\n <Table border>\n<tr><td>Level</td><td>Time</td><td>Logger</td><td>Function</td><td>Log Message</td><td>Stack trace</td></tr>\n");
+	}
+
+	public String getTail(Handler h) {
+		return ("</table>\n</body>\n</html>");
+	}
+}
