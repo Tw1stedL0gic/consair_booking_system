60e4ffa42657bd35244ca98f1f0b88ce8cae99a4
Carl Wingardh <c.a.wingardh@gmail.com>
2015-05-08 16:29:25 +0200

    Working on server prototype. Working on making one terminal handle 2 or more concurrent connecting terminals.

--- src/server_prototype.erl
+++ src/server_prototype.erl
@@ -1,5 +1,5 @@
 -module(server_prototype).
--export([start_send/2, start_recv/1, connect_send/3, connect_recv/2]).
+-export([start_send/2, start_recv/1, connect_send/3, connect_recv/2, connect/2, listen_loop/2, start_listen_loop/1]).
 
 
 -define(CONNECTIONOPTIONS, [binary, {packet, 4}, {active, false}]).
@@ -12,6 +12,38 @@
 %% Message: a bitstring. can be a normal string, but should be within << >> if something else. Example: <<1,1,1,1,1>>
 %% IP: IP of server. If using the same computer write "localhost" (including the quotation marks).
 
+
+
+
+
+
+start_listen_loop(Port) ->
+    %% open port
+    {ok, LSock} = gen_tcp:listen(Port, ?CONNECTIONOPTIONS),
+    start_listen_loop(LSock, 1).
+
+start_listen_loop(LSock, N) ->
+    %% connect to other device
+    {ok, Sock} = gen_tcp:accept(LSock),    
+    %% spawn process to handle this connection
+    NewPID = spawn(server_prototype, listen_loop, [Sock, N]),
+    io:fwrite("Number of connections: ~p, new process: ~p~n", [N, NewPID]),
+    start_listen_loop(LSock, N+1).
+    
+listen_loop(Sock, N) ->    
+    io:fwrite("C~p: Awaiting message...~n", [N]),
+    case gen_tcp:recv(Sock, 0, 10000) of
+	{error, timeout} -> 
+	    listen_loop(Sock, N),
+	    io:fwrite("...");
+	{error, Error} ->
+	    io:fwrite("C~p: Error: ~p~n", [N, {error, Error}]),
+	    {error, Error};
+	{ok, Package} ->
+	    io:fwrite("C~p: Message recieved: ~p~n", [N, Package]),
+	    listen_loop(Sock, N)
+    end.
+    
 
 start_send(Port, Message) ->
     Sock = start(Port),
@@ -40,5 +72,5 @@ connect_recv(IP, Port) ->
     Package.
 
 connect(IP, Port) ->
-    {ok, Sock} = gen_tcp:connect(IP, Port, ?CONNECTIONOPTIONS_AUX),
+    {ok, Sock} = gen_tcp:connect(IP, Port, ?CONNECTIONOPTIONS),
     Sock.
