a10b11ee2acda688aed8ea779e3a98e0883055a4
Lucas Arnström <lucasarnstrom@gmail.com>
2015-05-11 16:12:19 +0200

    Testing

--- src/JavaNetLib/src/main/java/ospp/bookinggui/networking/runnables/PacketListener.java
+++ src/JavaNetLib/src/main/java/ospp/bookinggui/networking/runnables/PacketListener.java
@@ -14,6 +14,8 @@ public class PacketListener implements Runnable {
 
 	private static final Logger logger = Logger.getLogger(PacketListener.class.getName());
 
+	public static volatile boolean debug = false;
+
 	private final Mailbox<Message>    mailbox;
 	private final BufferedInputStream input;
 
@@ -37,36 +39,38 @@ public class PacketListener implements Runnable {
 
 				logger.fine("Recieved data: " + Utils.bytePresentation(new int[] {data}));
 
-				// Header - Message Length
-				if(index < Message.HEADER_SIZE - 1) {
-					m_length |= (data << (8 * (3-index)));
-				}
-
-				// Header - Message ID
-				else if(index == Message.HEADER_SIZE - 1) {
-					// We are only interested in storing the body of the message.
-					message = new int[m_length - 1];
-					id = (short) (data & 0xff);
-				}
-
-				// Message body
-				else if(index < m_length + Message.HEADER_SIZE - 1) {
-					message[index - Message.HEADER_SIZE] = data;
-
-					// Finished, add message to inbox and reset accumulators!
-					if(index == m_length + Message.HEADER_SIZE - 2) {
-						logger.fine("Added message to inbox! MSG: " + Utils.bytePresentation(message));
-						mailbox.recieve(Message.parseMessage(id, message));
-
-						index = -1;
-						m_length = 0;
-						message = null;
-						id = 0;
+				if(!debug) {
+					// Header - Message Length
+					if(index < Message.HEADER_SIZE - 1) {
+						m_length |= (data << (8 * (3 - index)));
 					}
-				}
 
-				// Increment index counter
-				++index;
+					// Header - Message ID
+					else if(index == Message.HEADER_SIZE - 1) {
+						// We are only interested in storing the body of the message.
+						message = new int[m_length - 1];
+						id = (short) (data & 0xff);
+					}
+
+					// Message body
+					else if(index < m_length + Message.HEADER_SIZE - 1) {
+						message[index - Message.HEADER_SIZE] = data;
+
+						// Finished, add message to inbox and reset accumulators!
+						if(index == m_length + Message.HEADER_SIZE - 2) {
+							logger.fine("Added message to inbox! MSG: " + Utils.bytePresentation(message));
+							mailbox.recieve(Message.parseMessage(id, message));
+
+							index = -1;
+							m_length = 0;
+							message = null;
+							id = 0;
+						}
+					}
+
+					// Increment index counter
+					++index;
+				}
 			}
 
 			logger.severe("END OF STREAM!");
--- src/JavaNetLib/src/test/java/TestStartLib.java
+++ src/JavaNetLib/src/test/java/TestStartLib.java
@@ -1,18 +1,43 @@
 import org.junit.Test;
+import ospp.bookinggui.logging.ConsoleFormatter;
 import ospp.bookinggui.networking.Adapter;
 import ospp.bookinggui.networking.Mailbox;
 import ospp.bookinggui.networking.Message;
 import ospp.bookinggui.networking.NetworkAdapter;
+import ospp.bookinggui.networking.runnables.PacketListener;
 
 import java.io.IOException;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class TestStartLib {
 
+	private static final Logger root_logger = Logger.getLogger("");
+
 	@Test
 	public void start() throws IOException {
+
+		for(Handler h : root_logger.getHandlers()) {
+			if(h instanceof ConsoleHandler) {
+				h.setFormatter(new ConsoleFormatter());
+			}
+		}
+
+		root_logger.setLevel(Level.ALL);
+
+		PacketListener.debug = true;
+
+
 		Mailbox<Message> mailbox = new Mailbox<>();
-		Adapter adapter = new NetworkAdapter(mailbox, "130.243.180.55", 3333);
+
+		Adapter adapter = new NetworkAdapter(mailbox, "localhost", 3333);
 
 		adapter.disconnect();
+
+		while(true) {
+
+		}
 	}
 }
