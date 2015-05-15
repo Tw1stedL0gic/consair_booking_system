96670ad90037d9ac918e298558ee57010166f237
Lucas Arnström <lucasarnstrom@gmail.com>
2015-05-15 10:26:12 +0200

    finished most of GetPassengerListRespMsg

--- src/JavaNetLib/src/main/java/ospp/bookinggui/networking/messages/GetPassengerListRespMsg.java
+++ src/JavaNetLib/src/main/java/ospp/bookinggui/networking/messages/GetPassengerListRespMsg.java
@@ -5,12 +5,43 @@ import ospp.bookinggui.networking.Message;
 import java.io.UnsupportedEncodingException;
 
 public class GetPassengerListRespMsg extends Message {
-	public GetPassengerListRespMsg() {
+
+	private final long[] passenger_ids;
+
+	public GetPassengerListRespMsg(long[] ids) {
 		super(Type.GET_PASSENGERS_RESP);
+		this.passenger_ids = ids;
+	}
+
+	public static GetPassengerListRespMsg parse(byte[] body) {
+		if(body.length % 8 != 0) {
+			throw new IllegalArgumentException("The given body array does not have a length divisible by eight!");
+		}
+
+		long[] ids = new long[body.length / 8];
+
+		for(int i = 0; i < body.length; i++) {
+			ids[i / 8] = (ids[i / 8] << 8) | body[i];
+		}
+
+		return new GetPassengerListRespMsg(ids);
 	}
 
 	@Override
 	public byte[] constructBody() throws UnsupportedEncodingException {
-		return new byte[0];
+		byte[] body = new byte[8 * this.passenger_ids.length];
+
+		for(int i = 0; i < passenger_ids.length; i++) {
+			long paid = passenger_ids[i];
+			byte[] block = Message.createPaidBlock(paid);
+
+			System.arraycopy(block, 0, body, 8 * i, 8);
+		}
+
+		return body;
+	}
+
+	public long[] getPaidArray() {
+		return this.passenger_ids;
 	}
 }
