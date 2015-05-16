cf015875d450da4e1d38869054ba9540618237b8
Lucas Arnström <lucasarnstrom@gmail.com>
2015-05-16 17:28:48 +0200

    Changed contents of Passenger object

--- src/JavaNetLib/src/main/java/ospp/bookinggui/Passenger.java
+++ src/JavaNetLib/src/main/java/ospp/bookinggui/Passenger.java
@@ -2,48 +2,36 @@ package ospp.bookinggui;
 
 public class Passenger {
 	private final long   IDENTIFICATION;
-	private final String FIRST_NAME;
-	private final String SECOND_NAME;
-	private final String COUNTRY;
-	private final String ZIP_CODE;
-	private final String STREET_ADRESS;
-	private final int    HOUSE_NUMBER;
+	private final String NAME;
+	private final String ADDRESS;
+	private final String PAYMENT_INFO;
+	private final String EMAIL;
 
-	public Passenger(long id, String first_name, String second_name, String country, String zip_code, String street_adress, int house_number) {
+	public Passenger(long id, String name, String address, String payment_info, String email) {
 		this.IDENTIFICATION = id;
-		this.FIRST_NAME = first_name;
-		this.SECOND_NAME = second_name;
-		this.COUNTRY = country;
-		this.ZIP_CODE = zip_code;
-		this.STREET_ADRESS = street_adress;
-		this.HOUSE_NUMBER = house_number;
+		this.NAME = name;
+		this.ADDRESS = address;
+		this.PAYMENT_INFO = payment_info;
+		this.EMAIL = email;
 	}
 
 	public long getIdentification() {
 		return IDENTIFICATION;
 	}
 
-	public String getFirstName() {
-		return FIRST_NAME;
+	public String getName() {
+		return this.NAME;
 	}
 
-	public String getSecondName() {
-		return SECOND_NAME;
+	public String getAddress() {
+		return this.ADDRESS;
 	}
 
-	public String getCountry() {
-		return COUNTRY;
+	public String getPaymentInfo() {
+		return this.PAYMENT_INFO;
 	}
 
-	public String getZipCode() {
-		return this.ZIP_CODE;
-	}
-
-	public String getStreetAdress() {
-		return STREET_ADRESS;
-	}
-
-	public int getHouseNumber() {
-		return HOUSE_NUMBER;
+	public String getEmail() {
+		return this.EMAIL;
 	}
 }
