de9f4ec0e631de5425acb51456655c2668f6c9ce
Lucas Arnström <lucasarnstrom@gmail.com>
2015-05-18 14:34:48 +0200

    finished BookSeatRespMsg.parse()

--- src/JavaNetLib/src/main/java/ospp/bookinggui/networking/messages/BookSeatRespMsg.java
+++ src/JavaNetLib/src/main/java/ospp/bookinggui/networking/messages/BookSeatRespMsg.java
@@ -1,21 +1,41 @@
 package ospp.bookinggui.networking.messages;
 
+import ospp.bookinggui.exceptions.MalformedMessageException;
 import ospp.bookinggui.networking.Message;
 
 import java.io.UnsupportedEncodingException;
 
 public class BookSeatRespMsg extends Message {
-	public BookSeatRespMsg() {
+
+	private final BookSeatRespType TYPE;
+
+	public BookSeatRespMsg(BookSeatRespType type) {
 		super(Type.BOOK_SEAT_RESP);
+		this.TYPE = type;
 	}
 
-	public static BookSeatRespMsg parse(byte[] body) {
-		//TODO BookSeatRespMsg.parse()
-		return null;
+	public static BookSeatRespMsg parse(byte[] body) throws MalformedMessageException {
+		switch(body[0]) {
+			case 0x00:
+				return new BookSeatRespMsg(BookSeatRespType.SUCCESS);
+			case 0x10:
+				return new BookSeatRespMsg(BookSeatRespType.LOCKED);
+			case 0x20:
+				return new BookSeatRespMsg(BookSeatRespType.BOOKED);
+		}
+
+		throw new MalformedMessageException("The BookSeatRespMsg had an incorrect response type!");
 	}
 
 	@Override
 	public byte[] constructBody() throws UnsupportedEncodingException {
+		//TODO BookSeatRespMsg.constructBody()
 		return new byte[0];
+	}
+
+	public enum BookSeatRespType {
+		SUCCESS,
+		LOCKED,
+		BOOKED
 	}
 }
