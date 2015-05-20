c44f9f67855cc33d3315fece8f08b2098876027d
Carl Wingardh <c.a.wingardh@gmail.com>
2015-05-20 12:59:29 +0200

    Added documentation to booking_agent.erl to make it easier to code.

--- src/backend/booking_agent.erl
+++ src/backend/booking_agent.erl
@@ -30,30 +30,39 @@ disconnect(User) ->
 
 %%---------------------------------------------------------------------%%
 
-%% Returns a list of all airports. Return list of strings. 
+%% Returns a list of all airports. 
+%% Output: List of airport tuples. 
+%% Example: [{100, "ARN", "Arlanda"}, 
+%%           {101, "LAX", "Los Angeles International Airport"}]
 
 airport_list() ->
     ok. 
 
 %% In case of entering airport, return all airports which that airport
-%% has a route to
+%% has a route to. 
+%% Input: Airport ID
+%% Example: 100
+%% Output: List of airport tuples ({id, iata, airporrt_name}).
+%% Example: [{101, "LAX", "Los Angeles Internation Airport"}]
 
 airport_list(Airport) ->
     ok.
 
 %%---------------------------------------------------------------------%%
 
-%% @doc - Returns a list of all available flights
-
-flight_list() ->
-    %% return list of flights with departure time that has not passed
-    ok.
-
-%%---------------------------------------------------------------------%%
-
 %% @doc - Returns a list of flights between the two airports on that day. 
+%% Output: List of fligth tuples excluding arrival_date, flight_id and price. 
+%% ({id, airport, arrival_point, departure_date, flight_id, price})
+%% Example: [{1111,
+%%            {100, "ARN", "Arlanda"},
+%%            {101, "LAX", "Los Angeles International Airport"},
+%%            {2015,12,30},{11,59,59}}},
+%%           {1112,
+%%            {100, "ARN", "Arlanda"},
+%%            {101, "LAX", "Los Angeles International Airport"},
+%%            {{2015,12,30},{20,10,08}}}]
 
-route_search(AirportDeparture, AirportArrival, Date) ->
+route_search(airport, arrival_point, departure_date) ->
     %% search through database for route
     %% check if route exists
     %% check if date fits
@@ -61,7 +70,17 @@ route_search(AirportDeparture, AirportArrival, Date) ->
 
 %%---------------------------------------------------------------------%%
 
-%% @doc Returns all information about flight. 
+%% @doc Returns all information about flight.
+%% Input:  Flight ID
+%% Example: 100
+%% Output: Flight tuple
+%% Example: {1112,
+%%           {100, "ARN", "Arlanda"},
+%%           {101, "LAX", "Los Angeles International Airport"},
+%%           {{2015,12,30},{20,10,08}},
+%%           {{2015,12,31},{10,32,00}},
+%%           SEAT_TABLE,
+%%           ARNLAX120}
 
 flight_details(Flight) ->
     %% return all information about flight 
@@ -69,9 +88,43 @@ flight_details(Flight) ->
 
 %%---------------------------------------------------------------------%%
 
-%% @doc Returns whether a seat is booked, locked or avaliable. 
+%% @doc Returns information about seat(s). 
+%% When inputting a list of seat IDs, the output will be about the included seats. When inputting a row id, all seats in that row will be returned. When inputting a column id, all the seats in that column will be returned. 
+%% User information about lock is 0 = avail, 1 = reserved, 2 = reserved. 
+%% Input: Flight ID, Seat ID list OR Flight ID, Seat Column OR Flight ID, Seat Row. 
+%% Example: {101, A23}
+%% Output: Seat tuple ({id, flights, class, user, window, aisle, row, col, lock_s})
+%% Example: [{A23,
+%%            101,
+%%            1, 
+%%            carl,
+%%            0,
+%%            1,
+%%            A,
+%%            23,
+%%            2},
+%%           {B23,
+%%            101,
+%%            1, 
+%%            carl,
+%%            0,
+%%            1,
+%%            B,
+%%            23,
+%%            2}, ... ]
 
-seat_avaiability(Seat) ->
+seat_avaiability({Flight, Seat}, user) ->
+    ok. 
+
+%% @doc Returns information about seat(s), including admin info. 
+%% When inputting a list of seat IDs, the output will be about the included seats. When inputting a row id, all seats in that row will be returned. When inputting a column id, all the seats in that column will be returned. 
+%% Admin info about lock is 0 = avail, 1 = locked, 2 = reserved). 
+%% Input: Flight ID, Seat ID list OR Flight ID, Seat Column OR Flight ID, Seat Row. 
+%% Example: {101, [A23, B23, C23, D23]}
+%% Output: Seat tuple list ({id, flights, class, user, window, aisle, row, col, lock_s})
+%% Example: Same as above
+
+seat_avaiability({Flight, Seat}, admin) ->
     ok. 
 
 %%---------------------------------------------------------------------%%
