6a633e97729d9b3f79eb5e48c682f6da807f585d
Lucas Arnström <lucasarnstrom@gmail.com>
2015-05-28 17:21:52 +0200

    comments and deps

--- src/pivotgui/src/main/java/ospp/pivotgui/Main.java
+++ src/pivotgui/src/main/java/ospp/pivotgui/Main.java
@@ -6,16 +6,22 @@ import org.apache.pivot.wtk.*;
 
 public class Main implements Application {
 
+	public final Mailbox<Message> = new Mailbox<Message>();
 	private Window window = null;
 
 	public void startup(Display display, Map<String, String> map) throws Exception {
 		BXMLSerializer bxmlSerializer = new BXMLSerializer();
+
+		// Load main.bxml stylesheet into a Window-object
 		window = (Window) bxmlSerializer.readObject(Main.class, "/bxml/main.bxml");
 
+		// Retrieve the namespace of the loaded bxml file.
 		Map<String, Object> namespace = bxmlSerializer.getNamespace();
 
+		// Retrieve the button defined in the bxml file with the bxml:id="connectButton"
 		PushButton b = (PushButton) namespace.get("connectButton");
 
+		// Add event-listener, display message on button press.
 		b.getButtonPressListeners().add(new ButtonPressListener() {
 			@Override
 			public void buttonPressed(Button button) {
@@ -23,6 +29,7 @@ public class Main implements Application {
 			}
 		});
 
+		// Open the window.
 		window.open(display);
 	}
 
