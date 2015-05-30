a2b5a58a5bbaf3d2f0bae5ff551009869d7b3dd5
Carl Wingardh <c.a.wingardh@gmail.com>
2015-05-30 16:10:45 +0200

    Forgot another paren

--- src/backend/package_handler.erl
+++ src/backend/package_handler.erl
@@ -105,7 +105,7 @@ handle_package({?REQ_AIRPORTS, Airport_ID}, _) ->
     end;
 
 handle_package({?SEARCH_ROUTE, [Airport_A, Airport_B]}, _) ->
-    case booking_agent:route:search(Airport_A, Airport_B) of
+    case booking_agent:route_search(Airport_A, Airport_B) of
 	{ok, Flight_list} ->
 	    {ok, translate_package({?SEARCH_ROUTE_RESP, 
 				    lists:map(flatten_tuples_to_list, Flight_list)})};
