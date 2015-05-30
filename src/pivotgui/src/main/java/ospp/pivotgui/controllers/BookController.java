bf6b303a46e2f3e9936623bbbdf53fbe2e079241
Lucas Arnström <lucasarnstrom@gmail.com>
2015-05-30 12:33:55 +0200

    work on Book

--- src/pivotgui/src/main/java/ospp/pivotgui/controllers/BookController.java
+++ src/pivotgui/src/main/java/ospp/pivotgui/controllers/BookController.java
@@ -3,24 +3,44 @@ package ospp.pivotgui.controllers;
 import org.apache.pivot.beans.BXML;
 import org.apache.pivot.beans.Bindable;
 import org.apache.pivot.collections.ArrayList;
-import org.apache.pivot.collections.List;
+import org.apache.pivot.collections.HashMap;
 import org.apache.pivot.collections.Map;
 import org.apache.pivot.util.Resources;
+import org.apache.pivot.wtk.PushButton;
+import org.apache.pivot.wtk.TableView;
 import org.apache.pivot.wtk.Window;
 import ospp.bookinggui.Flight;
 
-import javax.swing.text.html.ListView;
 import java.net.URL;
 
 public class BookController extends Window implements Bindable {
 
 	@BXML
-	private ListView flightList = null;
+	private TableView  flightList   = null;
+	@BXML
+	private PushButton selectButton = null;
 
 	private Flight[] flights = null;
 
 	@Override
 	public void initialize(Map<String, Object> map, URL url, Resources resources) {
+
+		// Load flights into flightList
+		ArrayList<HashMap> tableRows = new ArrayList<>();
+
+		for(Flight f : flights) {
+			HashMap<String, String> row = new HashMap<>();
+
+			row.put("flightID", f.getFlightID());
+			row.put("flightNumber", f.getFlightNumber());
+			row.put("fromAirport", f.getFrom().getName());
+			row.put("toAirport", f.getTo().getName());
+			row.put("departure", f.getDeparture().toString());
+			row.put("arrival", f.getArrival().toString());
+		}
+
+		flightList.setTableData(tableRows);
+
 
 	}
 
