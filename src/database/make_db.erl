c66b2fe4b1f7109af54725ff5fec2bee937d572e
Omsala <osaka.sfi@gmail.com>
2015-06-03 23:52:05 +0200

    Update make_db.erl

--- src/database/make_db.erl
+++ src/database/make_db.erl
@@ -11,6 +11,9 @@
 %% TO BE ABLE TO MAKE THE DB WITH YOUR OWN PASSWORD  %%
 %%---------------------------------------------------%%
 
+%%@doc init
+%% Takes an password and makes the sturcture of the database on the mysql server.
+%%@end
 
 init(PASSWORD)->
     amnesia:db_tool(consair_database, [{make_hdr, "."},make_db, {dba_user, "root"}, {dba_password, PASSWORD}]).
