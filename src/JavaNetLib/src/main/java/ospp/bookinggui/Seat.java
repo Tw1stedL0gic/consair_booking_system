3e9d64d4f4dec7b341c5ce9dbe8cd30450a8a829
Lucas Arnström <lucasarnstrom@gmail.com>
2015-06-03 14:29:47 +0200

    fixed seat bug

--- src/JavaNetLib/src/main/java/ospp/bookinggui/Seat.java
+++ src/JavaNetLib/src/main/java/ospp/bookinggui/Seat.java
@@ -8,24 +8,30 @@ public class Seat {
 		ARG_AMOUNT = Seat.class.getDeclaredFields().length - 1;
 	}
 
-	private final String  SEAT_ID;
-	private final String  FLIGHT_ID;
-	private final String  KLASS;
-	private final boolean WINDOW;
-	private final int     ROW;
-	private final int     COL;
-	private       boolean locked;
+	private final String SEAT_ID;
+	private final String FLIGHT_ID;
+	private final String KLASS;
+	private final String USER;
+	private final String WINDOW;
+	private final String AISLE;
+	private final String ROW;
+	private final String COL;
+	private final String PRICE;
+	private final String LOCKED;
 
-	public Seat(String id, String flight_id, String klass, boolean window, int row, int col,
-				boolean locked) {
+	public Seat(String id, String flight_id, String klass, String user, String window, String aisle,
+				String row, String col, String price, String locked) {
 
 		this.SEAT_ID = id;
 		this.FLIGHT_ID = flight_id;
 		this.KLASS = klass;
+		this.USER = user;
 		this.WINDOW = window;
+		this.AISLE = aisle;
 		this.ROW = row;
 		this.COL = col;
-		this.locked = locked;
+		this.PRICE = price;
+		this.LOCKED = locked;
 	}
 
 	public static Seat parseBody(String[] parts, int offset) {
@@ -37,10 +43,13 @@ public class Seat {
 			parts[offset],
 			parts[offset + 1],
 			parts[offset + 2],
-			Boolean.valueOf(parts[offset + 3]),
-			Integer.valueOf(parts[offset + 4]),
-			Integer.valueOf(parts[offset + 5]),
-			Boolean.valueOf(parts[offset + 6])
+			parts[offset + 3],
+			parts[offset + 4],
+			parts[offset + 5],
+			parts[offset + 6],
+			parts[offset + 7],
+			parts[offset + 8],
+			parts[offset + 9]
 		);
 	}
 
@@ -56,46 +65,31 @@ public class Seat {
 		return this.KLASS;
 	}
 
-	public boolean getWindow() {
+	public String getUser() {
+		return this.USER;
+	}
+
+	public String getWindow() {
 		return this.WINDOW;
 	}
 
-	public int getRow() {
+	public String getAisle() {
+		return this.AISLE;
+	}
+
+	public String getRow() {
 		return this.ROW;
 	}
 
-	public int getCol() {
+	public String getCol() {
 		return this.COL;
 	}
 
-	public boolean isLocked() {
-		return this.locked;
+	public String getPrice() {
+		return this.PRICE;
 	}
 
-	/**
-	 * Set the locked value to true of this seat.
-	 * <p/>
-	 * NOTE! This does not send any data to the server!
-	 * It only alters the boolean value of this object.
-	 */
-	public void setLocked() {
-		this.locked = true;
-	}
-
-	/**
-	 * Set the locked value to false of this seat.
-	 * <p/>
-	 * NOTE! This does not send any data to the server!
-	 * It only alters the boolean value of this object.
-	 */
-	public void setUnlocked() {
-		this.locked = false;
-	}
-
-	public String[] createBody() {
-		return new String[]{
-			SEAT_ID, FLIGHT_ID, KLASS,
-			String.valueOf(WINDOW), String.valueOf(ROW), String.valueOf(COL), String.valueOf(locked)
-		};
+	public String getLocked() {
+		return this.LOCKED;
 	}
 }
